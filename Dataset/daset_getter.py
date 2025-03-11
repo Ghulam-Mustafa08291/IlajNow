4b395f25dad14d0c5395d4702c83864e51b1be0a
mustafa389 <140917530+mustafa389@users.noreply.github.com>
2025-03-11 08:36:14 +0500

    datasets

---
+++ Dataset/daset_getter.py
@@ -0,0 +1,20 @@
+# # import kagglehub
+
+# # # Download latest version
+# # path = kagglehub.dataset_download("rohanharode07/webmd-drug-reviews-dataset")
+
+# # print("Path to dataset files:", path)
+
+# import kagglehub
+
+# # Download latest version
+# path = kagglehub.dataset_download("choongqianzheng/disease-and-symptoms-dataset")
+
+# print("Path to dataset files:", path)
+
+import kagglehub
+
+# Download latest version
+path = kagglehub.dataset_download("riyanmujahid/doctors-in-pakistan-dataset")
+
+print("Path to dataset files:", path)
