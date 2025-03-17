deae50f472f3f86be55159c4dd98b38dfc49c126
mustafa389 <140917530+mustafa389@users.noreply.github.com>
2025-03-17 12:21:15 +0500

    start page(login) and DB connection and storage

--- maiin.py
+++ maiin.py
@@ -1,6 +1,25 @@
 # Import all the required libraries
-from PyQt6 import QtWidgets, uic, QtGui, QtCore
+from PyQt6 import QtWidgets, uic,QtGui, QtCore
 import sys
+import pyodbc
+from PyQt6.QtWidgets import QTableWidgetItem,QMessageBox
+
+
+server="DESKTOP-6J02DRS\SQLEXPRESS"
+database="IlajNow"
+use_windows_authentication=True
+username=""
+password=""
+
+if use_windows_authentication:
+    connection_string=f"DRIVER={{ODBC Driver 17 for SQL Server}};SERVER={server};DATABASE={database};Trusted_Connection=yes;"
+else:
+   connection_string= f"Driver={{ODBC Driver 17 for SQL Server}};SERVER={server};DATABASE={database};UID={username};PWD={password}"
+
+connection=pyodbc.connect(connection_string)
+
+cursor=connection.cursor()
+
 
 class UI(QtWidgets.QMainWindow):
     def __init__(self):
@@ -13,9 +32,32 @@ class UI(QtWidgets.QMainWindow):
         self.show()
         # Event Handling
         self.sign_up_button.clicked.connect(self.handle_click)
-
+        self.login_button.clicked.connect(self.handle_login)
+        
+    def handle_login(self):
+        entered_email=self.start_screen_email_field.text()
+        entered_password=self.start_screen_password_field.text()
+        cursor.execute(f"SELECT email FROM Users WHERE email = '{entered_email}' AND password = '{entered_password}'")    
+        rows=cursor.fetchall()
+        if len(rows)==0:
+            msg_box = QMessageBox()
+            msg_box.setWindowTitle('Login failed')
+            msg_box.setText('no user found!')
+            msg_box.exec()
+        else:
+          
+            print(rows)
+            msg_box = QMessageBox()
+            msg_box.setWindowTitle('Login passed')
+            msg_box.setText('user found!')
+            msg_box.exec()
+            
+            # self.home_screen=QtWidgets.QMainWindow() #window is initialized to none in the init function
+            # uic.loadUi("HOME.ui",self.home_screen)
+        
+        
     def handle_click(self):
-        self.lineEdit.setText("Welcome to QT Designer")
+        self.start_screen_email_field.setText("Welcome to QT Designer")
 
 app = QtWidgets.QApplication(sys.argv)  # Create an instance of QtWidgets.QApplication
 window = UI()  # Create an instance of our class
